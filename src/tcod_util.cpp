cd2a02cd493b3d0123cb3abdfb2dceacd169067e
Gavin Downard <gavin.downard@runbox.com>
2020-08-19 12:16:26 -0700

    Fix wall_merge, fixes gui bug

--- src/tcod_util.cpp
+++ src/tcod_util.cpp
@@ -4,6 +4,7 @@
 #include <cmath>
 #include <iostream>
 #include <numeric>
+#include <cassert>
 
 std::unique_ptr<TCODRandom> tcod_rand{};
 
@@ -32,44 +33,42 @@ bool percent_chance(double chance) {
 struct Wall_tile {
   bool n,s,w,e;
   bool operator==(const Wall_tile &o) const {return n==o.n && s==o.s && e==o.e && w==o.w;}
-  void operator+=(const Wall_tile &o) {n+=o.n; s+=o.s; e+=o.e; w+=o.w;}
+  Wall_tile &operator+=(const Wall_tile &o) {n+=o.n; s+=o.s; e+=o.e; w+=o.w; return *this;}
 };
 
 int wall_merge(int wall1, int wall2) {
-  std::array<int, 11> wall_chars
-    {
-     TCOD_CHAR_HLINE,
-     TCOD_CHAR_VLINE,
-     TCOD_CHAR_NE,
-     TCOD_CHAR_NW,
-     TCOD_CHAR_SE,
-     TCOD_CHAR_SW,
-     TCOD_CHAR_TEEW,
-     TCOD_CHAR_TEEE,
-     TCOD_CHAR_TEEN,
-     TCOD_CHAR_TEES,
-     TCOD_CHAR_CROSS,
-    };
-  std::array<Wall_tile, 11> wall_data
-    {
-     Wall_tile{0,0,1,1},
-     Wall_tile{1,1,0,0},
-     Wall_tile{0,1,1,0},
-     Wall_tile{0,1,0,1},
-     Wall_tile{1,0,1,0},
-     Wall_tile{1,0,0,1},
-     Wall_tile{1,1,1,0},
-     Wall_tile{1,1,0,1},
-     Wall_tile{1,0,1,1},
-     Wall_tile{0,1,1,1},
-     Wall_tile{1,1,1,1}
-    };
-  std::ptrdiff_t wall1_it = std::find(wall_chars.begin(), wall_chars.end(), wall1) - wall_chars.begin();
-  std::ptrdiff_t wall2_it = std::find(wall_chars.begin(), wall_chars.end(), wall2) - wall_chars.begin();
-  // using std::size_t;
-  // if (static_cast<size_t>(wall1_it) == wall_chars.size() || static_cast<size_t>(wall2_it) == wall_chars.size())
-  //   throw std::runtime_error{"wall_merge: invalid argument"};
-  Wall_tile result = wall_data[wall1_it];
-  result += wall_data[wall2_it];
+  std::array<int, 12> wall_chars {
+		' ',
+		TCOD_CHAR_HLINE,
+		TCOD_CHAR_VLINE,
+		TCOD_CHAR_NE,
+		TCOD_CHAR_NW,
+		TCOD_CHAR_SE,
+		TCOD_CHAR_SW,
+		TCOD_CHAR_TEEW,
+		TCOD_CHAR_TEEE,
+		TCOD_CHAR_TEEN,
+		TCOD_CHAR_TEES,
+		TCOD_CHAR_CROSS,
+	};
+  std::array<Wall_tile, 12> wall_data {
+		Wall_tile{0, 0, 0, 0},
+		Wall_tile{0, 0, 1, 1},
+		Wall_tile{1, 1, 0, 0},
+		Wall_tile{0, 1, 1, 0},
+		Wall_tile{0, 1, 0, 1},
+		Wall_tile{1, 0, 1, 0},
+		Wall_tile{1, 0, 0, 1},
+		Wall_tile{1, 1, 1, 0},
+		Wall_tile{1, 1, 0, 1},
+		Wall_tile{1, 0, 1, 1},
+		Wall_tile{0, 1, 1, 1},
+		Wall_tile{1, 1, 1, 1}
+	};
+  auto wall1_it = std::find(wall_chars.begin(), wall_chars.end(), wall1);
+  auto wall2_it = std::find(wall_chars.begin(), wall_chars.end(), wall2);
+  assert(wall1_it != wall_chars.end() && wall2_it != wall_chars.end());
+  Wall_tile result = wall_data[wall1_it - wall_chars.begin()];
+  result += wall_data[wall2_it - wall_chars.begin()];
   return wall_chars[std::find(wall_data.data(), wall_data.end(), result) - wall_data.begin()];
 }
